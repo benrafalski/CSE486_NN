da7011d4ff82a38479547744ca999055ce1fc102
DHoang14 <dhoang9@asu.edu>
2023-02-07 15:07:13 -0700

    changed to new dataset

--- new.py
+++ new.py
@@ -10,58 +10,80 @@ from sklearn.metrics import accuracy_score
 import nltk
 from nltk.tokenize import word_tokenize
 import matplotlib.pyplot as plt
+import json
+nltk.download('punkt')
 
 
-# nltk.download('punkt')
+#train_path = "data/Tweets.csv"
+#train_df = pd.read_csv(train_path)
+## train_df = train_df.sample(frac=1)
 
-train_path = "data/Tweets.csv"
-train_df = pd.read_csv(train_path)
-# train_df = train_df.sample(frac=1)
-
-train_df = train_df.drop(columns={"textID", "selected_text"})
+#train_df = train_df.drop(columns={"textID", "selected_text"})
 
 
-train_df, test_df = train_test_split(train_df, test_size=0.15)
+#train_df, test_df = train_test_split(train_df, test_size=0.15)
 
-remove_pos = 7312 - 6590
-remove_neut = 9456 - 6590
+#remove_pos = 7312 - 6590
+#remove_neut = 9456 - 6590
 
 
-neg_df = train_df[train_df["sentiment"] == "negative"] 
+#neg_df = train_df[train_df["sentiment"] == "negative"] 
 
-pos_df = train_df[train_df["sentiment"] == "positive"]
-neut_df = train_df[train_df["sentiment"] == "neutral"]
+#pos_df = train_df[train_df["sentiment"] == "positive"]
+#neut_df = train_df[train_df["sentiment"] == "neutral"]
 
-pos_drop_indices = np.random.choice(pos_df.index, remove_pos, replace=False)
-neut_drop_indices = np.random.choice(neut_df.index, remove_neut, replace=False)
+#pos_drop_indices = np.random.choice(pos_df.index, remove_pos, replace=False)
+#neut_drop_indices = np.random.choice(neut_df.index, remove_neut, replace=False)
 
-pos_undersampled = pos_df.drop(pos_drop_indices)
-neut_undersampled = neut_df.drop(neut_drop_indices)
+#pos_undersampled = pos_df.drop(pos_drop_indices)
+#neut_undersampled = neut_df.drop(neut_drop_indices)
 
-balanced_train_df = pd.concat([neg_df, pos_undersampled, neut_undersampled])
+#balanced_train_df = pd.concat([neg_df, pos_undersampled, neut_undersampled])
 
 
 
-# print(balanced_train_df["sentiment"].value_counts())
+## print(balanced_train_df["sentiment"].value_counts())
 
-train_clean_df, test_clean_df = train_test_split(balanced_train_df, test_size=0.15)
+#train_clean_df, test_clean_df = train_test_split(balanced_train_df, test_size=0.15)
 
-train_set = list(train_clean_df.to_records(index=False))
-test_set = list(test_clean_df.to_records(index=False))
+#train_set = list(train_clean_df.to_records(index=False))
+#test_set = list(test_clean_df.to_records(index=False))
 
-# print(train_set[:10])
+## print(train_set[:10])
 
-def remove_links_mentions(tweet):
-    link_re_pattern = "https?:\/\/t.co/[\w]+"
-    mention_re_pattern = "@\w+"
-    tweet = re.sub(link_re_pattern, "", tweet)
-    tweet = re.sub(mention_re_pattern, "", tweet)
-    return tweet.lower()
+#def remove_links_mentions(tweet):
+#    link_re_pattern = "https?:\/\/t.co/[\w]+"
+#    mention_re_pattern = "@\w+"
+#    tweet = re.sub(link_re_pattern, "", tweet)
+#    tweet = re.sub(mention_re_pattern, "", tweet)
+#    return tweet.lower()
 
-train_set = [(label, word_tokenize(remove_links_mentions(tweet))) for tweet, label in train_set]
-test_set = [(label, word_tokenize(remove_links_mentions(tweet))) for tweet, label in test_set]
+#train_set = [(label, word_tokenize(remove_links_mentions(tweet))) for tweet, label in train_set]
+#test_set = [(label, word_tokenize(remove_links_mentions(tweet))) for tweet, label in test_set]
 
-# print(test_set[:3])
+## print(test_set[:3])
+
+
+testdata = []
+for line in open('data/Software.json', 'r'):
+  testdata.append(json.loads(line))
+
+df = pd.DataFrame(testdata)
+df = df[["overall", "reviewText"]]
+
+##df.columns = ['overall', 'reviewText']
+##print(df.columns)
+print(df.head())
+df.replace([1.0, 2.0, 3.0], 0, inplace=True)
+df.replace(4.0, 1, inplace=True)
+df.replace(5.0, 2, inplace=True)
+
+df['reviewText'] = df['reviewText'].apply(lambda x: " ".join(x.lower() for x in str(x).split()))
+
+train_set, test_set = train_test_split(df.values.tolist(), test_size=0.15, random_state=64)
+train_df, valid_df = train_test_split(df.values.tolist(), test_size=0.15, random_state=64)
+
+
 
 index2word = ["<PAD>", "<SOS>", "<EOS>"]
 
@@ -77,13 +99,13 @@ word2index = {token: idx for idx, token in enumerate(index2word)}
 
 seq_length = 106
 
-def label_map(label):
-    if label == "negative":
-        return 0
-    elif label == "neutral":
-        return 1
-    else: #positive
-        return 2
+#def label_map(label):
+#    if label == "negative":
+#        return 0
+#    elif label == "neutral":
+#        return 1
+#    else: #positive
+#        return 2
 
 def encode_and_pad(tweet, length):
     sos = [word2index["<SOS>"]]
@@ -99,9 +121,9 @@ def encode_and_pad(tweet, length):
         truncated = encoded[:length - 2]
         return sos + truncated + eos
 
-train_encoded = [(encode_and_pad(tweet, seq_length), label_map(label)) for label, tweet in train_set]
+train_encoded = [(encode_and_pad(tweet, seq_length), label) for label, tweet in train_set]
 
-test_encoded = [(encode_and_pad(tweet, seq_length), label_map(label)) for label, tweet in test_set]
+test_encoded = [(encode_and_pad(tweet, seq_length), label) for label, tweet in test_set]
 
 batch_size = 50
 
@@ -110,12 +132,12 @@ train_y = np.array([label for tweet, label in train_encoded])
 test_x = np.array([tweet for tweet, label in test_encoded])
 test_y = np.array([label for tweet, label in test_encoded])
 
-train_ds = TensorDataset(torch.from_numpy(train_x), torch.from_numpy(train_y))
-test_ds = TensorDataset(torch.from_numpy(test_x), torch.from_numpy(test_y))
-
+train_ds = TensorDataset(torch.from_numpy(train_x), torch.from_numpy(train_y).type(torch.LongTensor))
+test_ds = TensorDataset(torch.from_numpy(test_x), torch.from_numpy(test_y).type(torch.LongTensor))
 
 train_dl = DataLoader(train_ds, shuffle=True, batch_size=batch_size, drop_last=True)
 test_dl = DataLoader(test_ds, shuffle=True, batch_size=batch_size, drop_last=True)
+
 
 
 class BiLSTM_SentimentAnalysis(torch.nn.Module) :

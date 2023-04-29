aa4eb6103d6d9842905443a530eee9db06677f8d
benrafalski <79751177+benrafalski@users.noreply.github.com>
2023-04-29 09:32:24 -0700

    Update bert.py
    updated bert

--- bert.py
+++ bert.py
@@ -1,37 +1,27 @@
+#!/usr/bin/python3
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 from transformers import pipeline
 import sys
 import pandas as pd
 from datasets import load_dataset, list_datasets
 
-# imdb = load_dataset("imdb")
-# print(imdb['train'][0])
-# sys.exit(42)
-
-# imdb = load_dataset("imdb")
-# sentiment_pipeline = pipeline("sentiment-analysis")
-model="finiteautomata/bertweet-base-sentiment-analysis" # 0.7974137931034483
-# model = "cardiffnlp/twitter-roberta-base-sentiment" # 0.7672413793103449
-# model = "bert-base-uncased" # 0.7629310344827587
-sentiment_pipeline = pipeline(model=model)
-df = pd.read_csv('data/Comments.csv')
-df = df[["Comment", "Sentiment"]]
-print(df.head())
+# get the pipeline model from transformers
+def get_pipeline(model):
+    return pipeline(model=model)
+# convert text to lowercase
 def low(text):
     return text.lower()
-
-df['Sentiment'] = df['Sentiment'].apply(low)
-print(df.head())
-
-features = df['Comment'].values
-labels = df['Sentiment'].values
-
-data = list(zip(features, labels))
-
-print(data[0])
-
-feat, label = data[0]
-
-
+# reads the data from the spreadsheet we made
+def get_data():
+    df = pd.read_csv('data/Comments.csv')
+    df = df[["Comment", "Sentiment"]]
+    df['Sentiment'] = df['Sentiment'].apply(low)
+    features = df['Comment'].values
+    labels = df['Sentiment'].values
+    data = list(zip(features, labels))
+    return data
+# encodes the labels for the model "finiteautomata/bertweet-base-sentiment-analysis" 
 def label_encoder(label):
     if label == "pos":
         return "label_2" 
@@ -39,25 +29,26 @@ def label_encoder(label):
         return "label_0"
     else:
         return "label_1"
+# test the accruacy of the pipeline using the data provided
+def test_pipiline(sentiment_pipeline, data):
+    total = 0
+    correct = 0
+    for f, l in data:
+        output = sentiment_pipeline(f)
+        expected = l.lower()[0:3]
+        pred = output[0]['label'].lower()
+        if expected == pred:
+            correct += 1
+        total += 1
+    return correct/total
 
+def main():
+    model="finiteautomata/bertweet-base-sentiment-analysis" # 0.7974137931034483
+    pipeline = get_pipeline(model=model)
+    data = get_data()
+    print("Starting testing...")
+    accuracy = test_pipiline(sentiment_pipeline=pipeline, data=data)
+    print(f'Model accracy is {accuracy}')
 
-total = 0
-correct = 0
-for f, l in data:
-    output = sentiment_pipeline(f)
-    # print(f"Expected {l} and got {output[0]['label']}")
-    # expected = label_encoder(l.lower()[0:3])
-    expected = l.lower()[0:3]
-    if expected == 'neu':
-        continue
-    # print(output)
-    # expected = l.lower()
-    pred = output[0]['label'].lower()
-    if expected == pred:
-        correct += 1
-    else:
-        print(f'Sentence: {f}')
-        print(f"\tExpected {expected} and got {pred}\n")
-    total += 1
-
-print(correct/total)
+if __name__ == "__main__":
+    main()
